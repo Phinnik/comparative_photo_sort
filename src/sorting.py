c8a846071a425ef1a3c01ff2af17aa321bb93af1
Phinnik <12518419+Phinnik@users.noreply.github.com>
2023-09-08 20:52:43 +0600

    sort only indices

--- src/sorting.py
+++ src/sorting.py
@@ -4,48 +4,55 @@ from typing import Optional
 class Sorter:
     def __init__(self, array: list) -> None:
         self.array = array
+        self.indices = list(range(len(array)))
 
     def _is_greater(self, left, right) -> bool:
         # TODO: make abstract
         return left > right
 
-    def _merge(self, left_idx: int, mid_idx: int, right_idx: int):
-        left_array = self.array[left_idx : mid_idx + 1]
-        right_array = self.array[mid_idx + 1 : right_idx + 1]
+    def _merge(self, start_idx: int, mid_idx: int, end_idx: int):
+        left_indices = self.indices[start_idx : mid_idx + 1]
+        right_indices = self.indices[mid_idx + 1 : end_idx + 1]
 
         i, j = 0, 0
-        k = left_idx
+        k = start_idx
 
-        while (i < len(left_array)) and (j < len(right_array)):
-            if self._is_greater(left_array[i], right_array[j]):
-                self.array[k] = right_array[j]
+        while (i < len(left_indices)) and (j < len(right_indices)):
+            left_index, right_index = left_indices[i], right_indices[j]
+            left_value = self.array[left_index]
+            right_value = self.array[right_index]
+            if self._is_greater(left_value, right_value):
+                self.indices[k] = right_index
                 j += 1
             else:
-                self.array[k] = left_array[i]
+                self.indices[k] = left_index
                 i += 1
             k += 1
 
-        while i < len(left_array):
-            self.array[k] = left_array[i]
+        while i < len(left_indices):
+            self.indices[k] = left_indices[i]
             i += 1
             k += 1
 
-        while j < len(right_array):
-            self.array[k] = right_array[j]
+        while j < len(right_indices):
+            self.indices[k] = right_indices[j]
             j += 1
             k += 1
 
     def sort(
         self,
-        left_idx: Optional[int] = None,
-        right_idx: Optional[int] = None,
-    ):
-        left_idx = left_idx if left_idx is not None else 0
-        right_idx = right_idx if right_idx is not None else len(self.array) - 1
+        start_idx: Optional[int] = None,
+        end_idx: Optional[int] = None,
+    ) -> list:
+        start_idx = start_idx if start_idx is not None else 0
+        end_idx = end_idx if end_idx is not None else len(self.array) - 1
 
-        if left_idx < right_idx:
-            mid_idx = left_idx + (right_idx - left_idx) // 2
+        if start_idx < end_idx:
+            mid_idx = start_idx + (end_idx - start_idx) // 2
 
-            self.sort(left_idx, mid_idx)
-            self.sort(mid_idx + 1, right_idx)
-            self._merge(left_idx, mid_idx, right_idx)
+            self.sort(start_idx, mid_idx)
+            self.sort(mid_idx + 1, end_idx)
+            self._merge(start_idx, mid_idx, end_idx)
+
+        sorted_list = [self.array[i] for i in self.indices]
+        return sorted_list
